f3de2a1ad31dc5b549bd283f6708b1ef97c9d575
nehajo88 <nehajo88@gmail.com>
2025-01-23 11:51:17 -0800

    Aligining layers to 10 x 10 degree grid

--- NASA_CMS_2023/Hunka_2024_NSD/Layer_Alignment/build_command_main_ADE.sh
+++ NASA_CMS_2023/Hunka_2024_NSD/Layer_Alignment/build_command_main_ADE.sh
@@ -6,8 +6,8 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P )
 conda env create -f ${basedir}/env_main_ADE.yml
 
 pushd ${HOME}
-source activate r
 
 # Do not remove this (PMM Dec 2022)
-# source activate icesat2_boreal
-# conda install -c conda-forge r-optparse r-ranger -y
+source activate icesat2_boreal
+source activate r
+conda install -c conda-forge r-optparse r-ranger -y
